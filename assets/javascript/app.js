3bbb71b1126ecd57272aa7fca681908cebcc38bf
Danielle Blaney <daniblaney@gmail.com>
2019-03-24 16:51:15 -0400

    audio added onload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,13 @@
 $(document).ready()
 
+  var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  if(!isChrome){
+    $('#iframeAudio').remove()
+  }
+  else{
+  $('#playAudio').remove() //just to make sure that it will not have 2x audio in the background
+  };
+
   var displayedButtons = ["Harry Potter", "Hermione Granger", "Ron Weasley", "Albus Dumbledore", "Rubeus Hagrid"];
 
   function displayImage(){
@@ -138,35 +146,3 @@ $(document).ready()
   setInterval(function() {
     walk();
   }, 1000);
-
-
-
-// $("button").on("click", function() {
-//   var person = $(this).attr("data-person");
-//   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-//     person + "&api_key=Pf6qkEL5g4Sla18HIcm9Ap9Ag0TLMpj8";
-
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   })
-//     .then(function(response) {
-//       var results = response.data;
-
-//       for (var i = 0; i < results.length; i++) {
-//         var gifDiv = $("<div>");
-
-//         var rating = results[i].rating;
-
-//         var p = $("<p>").text("Rating: " + rating);
-
-//         var personImage = $("<img>");
-//         personImage.attr("src", results[i].images.fixed_height.url);
-
-//         gifDiv.prepend(p);
-//         gifDiv.prepend(personImage);
-
-//         $("#gifs-appear-here").prepend(gifDiv);
-//       }
-//     });
-// });
